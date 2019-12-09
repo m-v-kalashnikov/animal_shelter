478d5c118944aa7250a9572fab40d4233f9233f4
Michael Kalashnikov <kalashnikovsystem@gmail.com>
2019-12-09 08:10:10 +0200

    Added allowed hosts

--- D6_project/settings.py
+++ D6_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1xhbow^gtcg!c0p58w31dwo30z-8-f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'afternoon-chamber-37539.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
